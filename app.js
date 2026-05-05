b591d678f9c8e87fb317c00d32c388e9822c1737
chandanvura <srichandanvura@gmail.com>
2026-05-05 20:19:29 +0530

    add node.js app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>DevOps Project - Live!</h1><p>Version 1.0</p>');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
