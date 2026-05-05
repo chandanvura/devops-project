8967418271d9c0c7089132463c6924041df682a3
chandanvura <srichandanvura@gmail.com>
2026-05-06 01:08:55 +0530

    version 2.0 full cicd test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>DevOps Project - Live!</h1><p>Version 1.1</p>');
+  res.send('<h1>DevOps Project - Live!</h1><p>Version 2.0</p>');
 });
 
 app.get('/health', (req, res) => {
