fd1aef1f00c35bdb399eab95534ce3a37ff4aca8
chandanvura <srichandanvura@gmail.com>
2026-05-05 21:40:44 +0530

    test webhook trigger v1.1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>DevOps Project - Live!</h1><p>Version 1.0</p>');
+  res.send('<h1>DevOps Project - Live!</h1><p>Version 1.1</p>');
 });
 
 app.get('/health', (req, res) => {
